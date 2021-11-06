f307d06a77d8a297daaf14a3b0b20027e9fc71e3
Fayaz Rafeek <fayazmrafeek@gmail.com>
2021-11-06 13:28:47 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+        {/* Remove this */}
+        <div className="w-screen h-screen flex items-center justify-center">
+          <div className="w-8/12 h-36 bg-purple-400 flex items-center justify-center rounded-md flex-wrap">
+              <h1 className='text-2xl px-6 py-3 font-bold bg-white'>ReactJs &amp; Tailwind is ready now ✨</h1>
+          </div>
+        </div>
+        {/*  */}
+
+
     </div>
   );
 }
